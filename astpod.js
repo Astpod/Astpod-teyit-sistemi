34ad3fd355899eb85fdf64e9f21766c90e2724a6
Astpod <49484285+Astpod@users.noreply.github.com>
2021-03-27 22:38:26 +0300

    Update astpod.js

--- astpod.js
+++ astpod.js
@@ -156,42 +156,6 @@ client.on("message", async (message) => {
     }
   }).catch();
       break;
-	  case "yasaklıtag":
-    let embed = new MessageEmbed().setColor('RANDOM').setTimestamp().setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
-    if (args[0] === "ekle") {
-        let data = await db.get(`yasaklitaglar.${message.guild.id}`) || []
-        let hedeftag = args[1]
-        if (!hedeftag) return message.channel.send(embed.setDescription(`${message.author}, Bir tag belirtin.`))
-        if (data.includes(hedeftag)) return message.channel.send(embed.setDescription(`Bu tag zaten yasaklı listede bulunmakta.`))
-        db.push(`yasaklitaglar.${message.guild.id}`, hedeftag)
-        message.channel.send(embed.setDescription(`
-        \`${hedeftag}\` Tagı yasaklı taglar listesine eklendi.
-        `))
-    }
-    if (args[0] === "liste") {
-        let data = await db.get(`yasaklitaglar.${message.guild.id}`) || []
-        message.channel.send(embed.setDescription(`
-**Yasaklı tag listesi;**
-\`${data.join('\n') || 'Bulunamadı.'} \`
-`))
-    }
-    if (args[0] === "kaldır") {
-        let data = await db.get(`yasaklitaglar.${message.guild.id}`) || []
-        let hedeftag = args[1]
-        if (!hedeftag) return message.channel.send(embed.setDescription(`${message.author}, Bir tag belirtin.`))
-        if (!data.includes(hedeftag)) return message.channel.send(embed.setDescription(`Bu tag zaten yasaklı listede bulunmamakta.`))
-        db.set(`yasaklitaglar.${message.guild.id}`, data.filter(x => x !== hedeftag))
-        message.channel.send(embed.setDescription(`
-    \`${hedeftag}\` Tagı yasaklı tag listesinden başarıyla kaldırıldı.
-`))
-    }
-    if (!args[0]) {
-        message.channel.send(embed.setDescription(`
-${message.author}, Hatalı kullanım Örnek\n \`.yasaklıtag ekle/kaldır/liste\`
-`))
-    }
-
-		 break
   }
 });
 
